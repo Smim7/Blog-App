b584f8bb8515e77c147a0327a6d7eab05b3cad3b
Smim7 <somayamorshed7@gmail.com>
2025-07-28 00:40:30 -0400

    Created entity class called Post and for this class created repository interface called PostRepository

---
+++ src/main/java/com/example/Blog_App/entity/Post.java
@@ -0,0 +1,23 @@
+package com.example.Blog_App.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name="posts",uniqueConstraints = {@UniqueConstraint(columnNames={"title"})}
+)
+public class Post {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+
+}
