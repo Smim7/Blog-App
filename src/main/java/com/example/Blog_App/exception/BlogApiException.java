b23b93b8eda5676665a0eb32c891aa9b578e057b
Smim7 <somayamorshed7@gmail.com>
2025-07-28 15:53:10 -0400

    created BlogApiException class

---
+++ src/main/java/com/example/Blog_App/exception/BlogApiException.java
@@ -0,0 +1,21 @@
+package com.example.Blog_App.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BlogApiException  extends RuntimeException{
+    private HttpStatus status;
+    private String message;
+
+    public BlogApiException(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
